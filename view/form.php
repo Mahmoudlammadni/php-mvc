c6841a28b215761ba62eaa4f96780a47c6590e2e
Mahmoud <lamm.mah2002@gmail.com>
2025-06-26 14:14:52 +0200

    feat:create the views to input user and to display message

---
+++ view/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Enter Your Name</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+        
+
+
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <input type="text" name="name" placeholder="Enter your name" class="form-control w-50 mt-5 ms-5"><br>
+        <input type="submit" name="ok" value="Click" class="btn btn-primary ms-5">
+    </form>
+</body>
+</html>
---
+++ view/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Greeting</title>
+</head>
+<body>
+    <h1><?php echo $message; ?></h1>
+    <a href="index.php">Go back</a>
+</body>
+</html>
